bcfe642a9ac10a6da8cdbd928ca70a13e74af2e0
David Simonelli <david8simonelli@gmail.com>
2015-08-10 15:12:34 -0500

    adding

--- database/migration.sql
+++ database/migration.sql
@@ -1,10 +1,41 @@
 USE list_db;
-DROP TABLE IF EXISTS "list_items";
-CREATE TABLE albums (
+DROP TABLE IF EXISTS "electronics";
+CREATE TABLE electronics(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    author_name VARCHAR(100) NOT NULL DEFAULT 'User Name',
+    user_name VARCHAR(100) NOT NULL DEFAULT 'User Name',
     sales DECIMAL(10,2) NOT NULL,
-    release_date INT NOT NULL,
-    section CHAR(100) NOT NULL,
+    publish_date INT NOT NULL,
+    PRIMARY KEY (id)
+);
+DROP TABLE IF EXISTS "furniture";
+CREATE TABLE furniture(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(100) NOT NULL DEFAULT 'User Name',
+    sales DECIMAL(10,2) NOT NULL,
+    publish_date INT NOT NULL,
+    PRIMARY KEY (id)
+);
+DROP TABLE IF EXISTS "cars";
+CREATE TABLE cars(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(100) NOT NULL DEFAULT 'User Name',
+    sales DECIMAL(10,2) NOT NULL,
+    publish_date INT NOT NULL,
+    PRIMARY KEY (id)
+);
+DROP TABLE IF EXISTS "clothes";
+CREATE TABLE clothes(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(100) NOT NULL DEFAULT 'User Name',
+    sales DECIMAL(10,2) NOT NULL,
+    publish_date INT NOT NULL,
+    PRIMARY KEY (id)
+);
+DROP TABLE IF EXISTS "pets";
+CREATE TABLE pets(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(100) NOT NULL DEFAULT 'User Name',
+    sales DECIMAL(10,2) NOT NULL,
+    publish_date INT NOT NULL,
     PRIMARY KEY (id)
 );
