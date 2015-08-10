c419b7b1c0acd7d5fc55dfcb49702fe974a5bd9e
Dylan Perenchio <perenchiod@gmail.com>
2015-08-10 14:15:20 -0500

    Added html to the index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,32 @@
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Marketing Homepage</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="../viwes/partials/header.php">
+	<link rel="stylesheet" type="text/css" href="../viwes/partials/footer.php">
+	<link rel="stylesheet" type="text/css" href="../viwes/partials/navbar.php">
 </head>
 <body>
 
+	<div class="electronics">
+		<a rel="stylesheet" type="text/css" href="electronics">Electronics</a>
+	</div>
+
+	<div class="furniture">
+		<a rel="stylesheet" type="text/css" href="furniture">Furniture</a>
+	</div>
+
+	<div class="cloths">
+		<a rel="stylesheet" type="text/css" href="cloths">Cloths</a>
+	</div>
+
+	<div class="cars">
+		<a rel="stylesheet" type="text/css" href="cars">Cars</a>
+	</div>
+
+	<div class="pets">
+		<a rel="stylesheet" type="text/css" href="pets">Pets</a>
+	</div>
 </body>
 </html>
