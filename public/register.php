9f81fd8453a1668476f1e4d28516ff56f80802ce
Dylan Perenchio <perenchiod@gmail.com>
2015-08-11 13:47:27 -0500

    Addes some util files and also made some database changes

--- public/register.php
+++ public/register.php
@@ -1,6 +1,20 @@
 <?php
     require_once'../views/partials/navbar.php';
     require_once'../views/partials/footer.php';  
+    require_once'../utils/input.php';
+    
+    if(inputHas('username') && inputHas('email') && inputHas('password')) {
+        
+        
+    }
+    define('DB_HOST','127.0.0.1');
+    define('DB_NAME','users_db');
+    define('DB_USER', );
+    define('DB_PASS','');
+    $dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+    $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
 ?>
 <!DOCTYPE html>
 <html>
