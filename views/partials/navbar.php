f2786e93d2ad60de268f3c040c8b8f9b1ebfdd3e
David Simonelli <david8simonelli@gmail.com>
2015-08-11 11:41:51 -0500

    Fixed comments

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -6,8 +6,8 @@
 	<a id='log' href="http://missingonecharecte.dev/login.php">Log In</a>
 	<a href="http://missingonecharecte.dev/register.php">Register</a>
 	<a href="http://missingonecharecte.dev/help.php">Help</a>
-	<a href="https://twitter.com/David8Simonelli" class="twitter-follow-button" data-show-count="false">Follow @David8Simonelli</a>
-	<div class="fb-like" data-href="https://www.facebook.com/david.simonelli.9" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+	<a id='test' href="https://twitter.com/David8Simonelli" class="twitter-follow-button" data-show-count="false">Follow @David8Simonelli</a>
+	<div id='testtwo' class="fb-like" data-href="https://www.facebook.com/david.simonelli.9" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
 	<div id="fb-root"></div>
 </div>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
