0dda22db5d2bdd907b9879db050936489357a00d
David Simonelli <david8simonelli@gmail.com>
2015-08-12 15:43:05 -0500

    login

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -7,7 +7,7 @@
 	<a href="http://missingonecharecte.dev/register.php">Register</a>
 	<a href="http://missingonecharecte.dev/help.php">Help</a>
 	<a id='test' href="https://twitter.com/David8Simonelli" class="twitter-follow-button" data-show-count="false">Follow @David8Simonelli</a>
-	<div id='testtwo' class="fb-like" data-href="https://www.facebook.com/david.simonelli.9" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+	<div id='testtwo' class="fb-like" data-href="https://www.facebook.com/missingonecharecte?pnref=lhc" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
 	<div id="fb-root"></div>
 </div>
 </div>
