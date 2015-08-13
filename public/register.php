05057ff6f835d48edbc5a39d361f112d3298858f
Dylan Perenchio <perenchiod@gmail.com>
2015-08-13 14:29:19 -0500

    Ok so i forgot about the idea to make users in mysql for all of the regestered users, but i got the login to work with the hashed passwords here

--- public/register.php
+++ public/register.php
@@ -36,10 +36,6 @@
             $password = escapeVar(inputGet('password'));
             $email = escapeVar(inputGet('email'));
             $_SESSION['databasePassword'] = $password;
-            $userDatabase = "FLUSH PRIVILEGES; CREATE USER '$username'@'localhost' IDENTIFIED BY '$_SESSION[databasePassword]';
-            GRANT SELECT ON listed_items TO '$username'@'localhost';
-            GRANT SELECT ON users TO '$username'@'localhost';";
-            $dbc->exec($userDatabase);
             $password = password_hash($password , PASSWORD_DEFAULT);
 
             if(inputHas('first_name') || inputHas('last_name')) {
--- utils/Auth.php
+++ utils/Auth.php
@@ -5,14 +5,13 @@
 			protected static function getInfo($username,$password) {
 				$setPassword = $password;
 				
-				$userDBinfo = new mysqli("127.0.0.1", $_SESSION['username'], $_SESSION['databasePassword'], "users");
+				$userDBinfo = new mysqli("127.0.0.1", "root", "", "list_db");
 				$usernameVar = mysqli_query($userDBinfo , "SELECT * FROM users WHERE username = '$username'");
 				$queryHash = mysqli_query($userDBinfo , "SELECT password FROM users WHERE password = '$password'");
 				$hashedPassword = mysqli_fetch_row($queryHash);
 		        $result = mysqli_num_rows($usernameVar);
 		        if($result > 0) { 
-					if(password_verify($_SESSION['password'] , '$2y$10$o8LyYT3JXY5ODGs7OgBZ7uink3xZ024MQ1ucjyBiAUS1pV4UvSw7K')){ 
-						self::loggedInDB($_SESSION['username'] , $_SESSION['databasePassword']);
+					if(password_verify($_SESSION['password'] , '$2y$10$fmtLDBsMA56PBGlakVMouu2N8DcpMeC5dQl6Eg64GP62jLoUNCDPm')){ 
 						echo "<script type='text/javascript'> alert('yeah!'); </script>";
 					}
 
@@ -53,15 +52,7 @@
 				exit();
 			}
 
-			protected static function loggedInDB($username , $password) {
-				define('DB_HOST1','127.0.0.1');
-			    define('DB_NAME1','listed_items');
-			    define('DB_USER1', $username);
-			    define('DB_PASS1', $password);
-			    $dbc2 = new PDO('mysql:host='.DB_HOST1.';dbname='.DB_NAME1, DB_USER1, DB_PASS1);
-			    $dbc2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			}
-
+			
 	}
 
 ?>
